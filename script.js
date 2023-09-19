09b853a17942485e7f8db50216fb0168a31b0c83
santiagocadavid <santycadavid@hotmail.com>
2023-09-19 11:32:43 -0500

    Se agregan todos los productos por categoria

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
         video.appendChild(videoElement);
     });
 });
+
+    // JavaScript for the slideshow
+    const words = document.querySelectorAll('.word');
+    let currentWordIndex = 0;
+
+    function showNextWord() {
+    words[currentWordIndex].style.display = 'none';
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+    words[currentWordIndex].style.display = 'block';
+    }
+
+    // Initial display
+    words[currentWordIndex].style.display = 'block';
+
+    // Set the interval for showing the next word (change every 3 seconds)
+    setInterval(showNextWord, 3000);
